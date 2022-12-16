b2e7d6a916f8645076c569ec8ddd0d547694f2d9
escar1227 <112179110+escar1227@users.noreply.github.com>
2022-12-15 22:15:02 -0500

    Delete Final Project directory

--- Final Project/js/gallery.js
+++
@@ -1,4 +0,0 @@
-var fullImgBox = document.getElementById("fullImgBox");
-var fullImg = document.getElementById("fullImg");
-function openFullImg(pic) {"use strict"; fullImgBox.style.display = "flex"; fullImg.src = pic; }
-function closeFullImg() {"use strict"; fullImgBox.style.display = "None"; }
